b91b20539dd0082f8205728b9723d6dd14c20593
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-03-23 15:25:30 +0500

    Update fahrenheit_to_celcuise.py

---
+++ intro_python/fahrenheit_to_celcuise.py
@@ -0,0 +1,6 @@
+print("Covert Fahrenheit to Celcius")
+
+fahrenheit = float(input("Enter the temperature in Fahrenheit: "))
+celcuis = (fahrenheit - 32) * 5.0/9.0
+
+print(f"The {fahrenheit} fahrenheit is equal to {celcuis} celcius")
