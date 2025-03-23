0449409212ec6a7aa93ade863feb065f5ff34277
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-03-23 16:08:17 +0500

    Create how_old_are_they.py

---
+++ intro_python/how_old_are_they.py
@@ -0,0 +1,20 @@
+Anton = 21 # Anton is 21 years old
+
+Beth = Anton + 6 # Beth is 6 years older than Anton
+
+Chen = Beth + 20 # Chen is 20 years older than Beth
+
+Drew = Chen + Anton # Drew is as old as Chen and Anton combined
+
+Ethan = Chen  # Ethan is as old as Chen
+
+
+print(f"Anton is {Anton} years old")
+
+print(f"Beth is {Beth} years old")
+
+print(f"Chen is {Chen} years old")
+
+print(f"Drew is {Drew} years old")
+
+print(f"Ethan is {Ethan} years old")
