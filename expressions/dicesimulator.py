5f9b56f2179c5ef6000c516290ca67b5abe906b8
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-03-23 17:00:52 +0500

    Create dicesimulator.py

---
+++ expressions/dicesimulator.py
@@ -0,0 +1,15 @@
+import random
+
+dice_sides = 6
+
+die1 = random.randint(1, dice_sides)
+die2 = random.randint(1, dice_sides)
+die3 = random.randint(1, dice_sides)
+
+print("die1",die1)
+print("die2",die2)
+print("die3",die3)
+
+# def roll_dice():
+#     die1 = random.randint(1, dice_sides)
+#     die2 = random.randint(1, dice_sides)
