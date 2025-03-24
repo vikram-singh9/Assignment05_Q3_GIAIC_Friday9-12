3ccb808e7bf1bea1ee2e8d0008412b756b07f181
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-03-24 14:44:08 +0500

    Update e=mc2.py

---
+++ expressions/e=mc2.py
@@ -0,0 +1,8 @@
+# Speed of light constant
+c = 299792458  # meters per second
+
+m = float(input("Enter mass in kg: "))
+
+energy = m * c ** 2
+
+print(f"The {m}kg is equivalent to {energy} Joules of energy.")
