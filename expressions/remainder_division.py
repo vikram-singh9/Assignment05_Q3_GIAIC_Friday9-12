2ee9ec3a7c3939b200f39e2596bad12e844c6ff6
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-03-24 17:58:30 +0500

    updated

--- expressions/remainder_division.py
+++ expressions/remainder_division.py
@@ -1,1 +1,10 @@
-opp
+def main():
+    while True:
+        number1 = int(input("Enter the first number: "))
+        number2 = int(input("Enter the second number: "))
+        result = number1 / number2
+        remainder = number1 % number2
+        print(f"The result of this division is {result} and remainder is {remainder}")
+
+if __name__ == "__main__":
+    main()
---
+++ expressions/roll_dice.py
@@ -0,0 +1,14 @@
+import random
+
+def roll_dice():
+    dice_rolls = 6
+
+    die1 = random.randint(1,dice_rolls)
+    die2 = random.randint(1,dice_rolls)
+
+    sum = die1 + die2
+
+    print(f"The sum of die {die1} and die {die2} is {sum}")
+
+if __name__ == "__main__":
+    roll_dice()
