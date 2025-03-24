2a2ed4be8e213fe96ac2abd71e23372209d6479c
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-03-24 17:59:03 +0500

    Update roll_dice.py

--- expressions/roll_dice.py
+++ expressions/roll_dice.py
@@ -8,7 +8,7 @@ def roll_dice():
 
     sum = die1 + die2
 
-    print(f"The sum of die {die1} and die {die2} is {sum}")
+    print(f"The sum of die {die1} and die {die2} is {sum}.")
 
 if __name__ == "__main__":
     roll_dice()
