732d262fe300c39a7beb524355ddae37575a32ab
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-03-25 18:04:00 +0500

    Create leep_year.py

---
+++ if_statements/leep_year.py
@@ -0,0 +1,9 @@
+
+while True:
+    print("Enter a year: ")
+    year  = int(input())
+
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        print(f"{year} is a leap year")
+    else:
+        print(f"{year} is not a leap year")
