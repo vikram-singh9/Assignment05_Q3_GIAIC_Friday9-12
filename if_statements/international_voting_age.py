38bb4aa301afd663fee18ef6026ec6c4811c3522
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-03-25 17:53:21 +0500

    Update international_voting_age.py

--- if_statements/international_voting_age.py
+++ if_statements/international_voting_age.py
@@ -3,7 +3,22 @@ india  = 20
 pakistan = 18
 
 def main():
-    if 
+    user_age = int(input("Enter your age: ") )   
+    if user_age >= brazil:
+        print(f"You CAN vote in brazil where voting age is {brazil}")
+    else:
+        print(f"You can not vote in brazil where voting age is {brazil}")
+    
+    if user_age >= india:
+        print(f"You CAN vote in india where voting age is {india}")
+    else:
+        print(f"You can not vote in india where voting age is {india}")
+
+
+    if user_age >= pakistan:
+        print(f"You CAN vote in pakistan where voting age is {pakistan}")   
+    else:
+        print(f"You can not vote in pakistan where voting age is {pakistan}")
    
 if __name__ == '__main__':
     main()
