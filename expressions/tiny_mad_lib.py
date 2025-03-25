7a3b42f64adc227711427e4329b1c1ae5b0b6c27
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-03-25 14:44:56 +0500

    Update tiny_mad_lib.py

---
+++ expressions/tiny_mad_lib.py
@@ -0,0 +1,12 @@
+# tiny mad lib game
+
+def mad_lib_game():
+    adjective = input("Enter an adjective: ")
+    noun = input("Enter a noun: ")
+    verb = input("Enter a verb: ")
+    print(f"Coding is so {adjective}! It makes me so excited all the time because I love to {verb}. Stay hydrated and {verb} like you are a {noun}!")
+
+
+
+if __name__ == "__main__":
+    mad_lib_game()
