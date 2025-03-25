da33961c4de3ba9ac6b60956a677a3f286eb784c
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-03-25 15:00:45 +0500

    Update add_many_numbers.py

--- lists/add_many_numbers.py
+++ lists/add_many_numbers.py
@@ -1,10 +1,9 @@
 def sum_of_numbers(numbers):
-    total = 0  # Shuru mein sum zero se start karenge
-    for num in numbers:  # Har number ko list se lenge
-        total = total + num  # Total mein add karte jayenge
-    return total  # Last mein total return karenge
+    total = 0  
+    for num in numbers: 
+        total = total + num  # Har number ko total mein add karte jayenge
+    return total 
 
-# Test karne ke liye
 my_list = [1, 2, 3, 4]
 result = sum_of_numbers(my_list)
 print(f"Sum of {my_list} is: {result}")
