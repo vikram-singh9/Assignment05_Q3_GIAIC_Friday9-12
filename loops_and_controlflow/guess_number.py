b7beb09d5bd724524df2ba794c0e3a870e353c76
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-03-27 23:00:47 +0500

    Create guess_number.py

---
+++ loops_and_controlflow/guess_number.py
@@ -0,0 +1,8 @@
+def main():
+    print("Delete this line and write your code here! :)")
+
+
+# This provided line is required at the end of
+# Python file to call the main() function.
+if __name__ == '__main__':
+    main()
