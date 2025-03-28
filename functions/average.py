d72587a61762eeb1754743b0fa71c0b65b0c6cff
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-03-29 01:50:17 +0500

    Update average.py

---
+++ functions/average.py
@@ -0,0 +1,9 @@
+def average():
+    num1 = int(input("Enter number one: "))
+    num2 = int(input("Enter number two: "))
+
+    average = (num1 + num2) / 2
+    print(f"The average of {num1} and {num2} is {average}")
+
+if __name__ == '__main__':
+    average()
