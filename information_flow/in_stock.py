928bcfa160b428cde4573ba9e721f16944c59345
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-03-29 02:15:40 +0500

    Update in_stock.py

--- information_flow/in_stock.py
+++ information_flow/in_stock.py
@@ -4,9 +4,13 @@ def num_in_stock(fruit):
 def main():
     fruit = input("Enter a fruit: ")
     count = num_in_stock(fruit)
-    if count > 0:
-        print(count)
-    else:
+    if count == 0:
         print("This fruit is not in stock.")
+    elif count == 1:
+        print("There is 1 fruit in stock.")
+    elif count > 1:
+        print(f"There are {count} fruits in stock.")
+    
 
-main()
+if __name__ == '__main__':
+    main()
