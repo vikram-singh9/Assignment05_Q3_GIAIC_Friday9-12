a94eac4d6d5fde4fd2212830a72615a0fec9d0ed
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-03-30 02:55:24 +0500

    Update main.py

--- project-password_generator_app/main.py
+++ project-password_generator_app/main.py
@@ -1,23 +1,8 @@
-import streamlit as st
-import random 
+import random
 import string
 
-def password_generator (length, use_digits, use_special):
-    charactors = string.ascii_letters # a to z and A TO Z
-    if use_digits:
-        charactors += string.digits
+def password_generator(length):
+    characters = string.ascii_letters + string.digits + "!@#$%^&*"
+    return "".join(random.choice(characters) for _ in range(length))
 
-    if use_special:
-        charactors += string.punctuation
-    
-    return "".join(random.choice(charactors) for _ in range(length))
-
-
-st.title("🤠Password Generator Poject👀")
-length = st.slider("Select passwprd range", min_value=5, max_value=30, value=8)
-use_digit = st.checkbox("Add digits")
-use_special = st.checkbox("Add Special Charactors")
-
-if st.button(f"Generate Password"):
-    password = password_generator(length, use_digit, use_special)
-    st.write(password)
+if 
