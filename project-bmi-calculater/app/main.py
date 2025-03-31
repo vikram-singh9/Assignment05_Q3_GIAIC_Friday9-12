fd7786a7a9a5c2c3c18065397c0a530f81604906
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-04-01 01:15:17 +0500

    Update main.py

--- project-bmi-calculater/app/main.py
+++ project-bmi-calculater/app/main.py
@@ -20,10 +20,13 @@ if __name__ == "__main__":
           
             if bmi < 18.5:
                 st.warning("You are underweight")
+                st.balloons()
             elif 18.5 <= bmi < 25:
                 st.success("You have a normal weight")
+                st.balloons()
             elif 25 <= bmi < 30:
                 st.warning("You are overweight")
+                st.balloons()
             else:
                 st.error("You are obese")
         else:
