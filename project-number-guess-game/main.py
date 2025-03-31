6f759058d457f95f4bb38a0aec501160fe09215b
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-04-01 00:03:54 +0500

    Update main.py

--- project-number-guess-game/main.py
+++ project-number-guess-game/main.py
@@ -1,11 +1,11 @@
 import random
 
-user = int(input("enter your guess (1 to 10): "))
-
 computer = random.randint(1,10)
 while True:
+    user = int(input("enter your guess (1 to 10): "))
     if user == computer:
         print(f"Correct the number was {computer}")
         break
     else:
         print("try again...")
+       
