a67a659f198297687d40f80c8f53a628ba55a457
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-03-31 22:47:01 +0500

    Update main.py

--- project-password_generator_app/main.py
+++ project-password_generator_app/main.py
@@ -5,6 +5,7 @@ def password_generator(length):
     characters = string.ascii_letters + string.digits + "!@#$%^&*"
     return "".join(random.choice(characters) for _ in range(length))
 
+# Example usage
 if __name__ == "__main__":
     print(password_generator(10))
 
