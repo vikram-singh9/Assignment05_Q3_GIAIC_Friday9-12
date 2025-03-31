88b7fcdd960eb3417003cd32b7de4a0f01e12ab6
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-03-31 23:52:12 +0500

    Update main.py

--- project-mad-libs/main.py
+++ project-mad-libs/main.py
@@ -1,13 +1,26 @@
 noun = input("Enter the noun")
+verb = input("Enter the verb")
+adjective = input("Enter the adjective")
 
-# Print the completed story
+story = print(f"""
+A Crazy Adventure
+
+One day, a {adjective} {noun} decided to {verb} in the {adjective} park.
+Suddenly, a {adjective} animal appeared and started to {verb}.
+The {noun} was so {adjective} that it {verb} into a {adjective} place.
+
+After that, a {adjective} vikram arrived and said, "aaaen!"
+Everyone {verb} and had a {adjective} day!
+""")
+
+
 print("\nHere's your Mad Libs story:")
 print(story)
 
 # Ask if they want to play again
-play_again = input("\nWould you like to play again? (yes/no): ")
+
+play_again = input("Do you like to play again: (yes/no)")
 if play_again.lower() == "yes":
-    print("\nLet's play again!\n")
-    # The game would restart here
+    print("let's play again!")
 else:
-    print("\nThanks for playing Mad Libs! Goodbye!")
+    print("don't forget to come back")
