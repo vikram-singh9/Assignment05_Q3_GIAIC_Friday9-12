141bcd5560474d2bea47af10d1ef72bb99c69d19
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-04-01 01:04:43 +0500

    Update main.py

--- project-bmi-calculater/app/main.py
+++ project-bmi-calculater/app/main.py
@@ -10,16 +10,16 @@ if __name__ == "__main__":
     st.title("BMI Calculator")
     st.write("Enter your height and weight to calculate your BMI")
 
-    # Get user inputs
+ 
     weight = st.number_input("Enter your weight (in kg)", min_value=1.0, max_value=500.0, value=0)
     height = st.number_input("Enter your height (in meters)", min_value=0.1, max_value=3.0, value=0)
 
-    # Calculate BMI when button is pressed
+
     if st.button("Calculate BMI"):
         bmi = weight / (height ** 2)
         st.write(f"Your BMI is: {bmi:.2f}")
         
-        # Display BMI category
+        
         if bmi < 18.5:
             st.warning("You are underweight")
         elif bmi < 25:
@@ -29,7 +29,7 @@ if __name__ == "__main__":
         else:
             st.error("You are obese")
             
-        # Display BMI chart
+    
         st.write("\nBMI Categories:")
         st.write("Underweight = <18.5")
         st.write("Normal weight = 18.5–24.9") 
